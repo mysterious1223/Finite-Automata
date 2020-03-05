7f4cd4f871114c9d01dc993e28c862123c2c81bd
Kevin Singh <kevinsingh.developer@gmail.com>
2020-03-04 23:21:47 -0500

    Update FiniteAutoTool.py
    Doesn't let state input have spaces A,B, C,D

--- FiniteAutoTool/FiniteAutoTool.py
+++ FiniteAutoTool/FiniteAutoTool.py
@@ -4,21 +4,29 @@ import sys
 class FA_String_Parser:
 
     #convert string to array
+    @staticmethod
     def ParseToList (delimeter,inputString):
         mylist = inputString.split(delimeter)
         return mylist
     # before parsing lets make sure there arent dupes in the string
+    @staticmethod
     def CheckStringForDuplicate (delimeter, inputString):
         mylist = inputString.split(delimeter)
+        length = len(mylist) 
+   
+        for i in range(length): 
+            mylist[i] = mylist[i].strip()
+
         return (len(mylist) != len(set(mylist)))
 
 
-
+    @staticmethod
     def CheckIfListContainsString (myString, myList):
         for i in myList:
             if myString == i:
                 return True
         return False
+    @staticmethod
     def CheckIfListContainsList (myStringList, myList):
         count = 0
         for i in myStringList:
@@ -30,6 +38,8 @@ class FA_String_Parser:
             return True
         else:
             return False
+
+            
 class FiniteAuto:
 
     states = []
@@ -47,11 +57,6 @@ class FiniteAuto:
     
 
 
-states = []
-alphabet = []
-startpoint = ""
-AcceptStates = []
-
 def init_call ():
 
     print ("********* Initializing Finite ***********")
@@ -65,12 +70,14 @@ def init_call ():
         print ("[!] Duplicate states please enter unique states!")
         init_call ()
 
+
     states = FA_String_Parser.ParseToList (",",s_states)
 
 
     if (len(states) < 2):
         print ("[!] Please enter more than one states")
         init_call ()
+
 
     #print (str(len(states)))
 
@@ -81,6 +88,7 @@ def init_call ():
     if (FA_String_Parser.CheckStringForDuplicate (",",s_alphabet)):
         print ("[!] Duplicate alphabet please enter unique alphabet!")
         init_call ()
+
     alphabet = FA_String_Parser.ParseToList (",",s_alphabet)
 
 
@@ -93,6 +101,7 @@ def init_call ():
         print ("[!] State was not found in state list!")
         init_call ()
 
+
     s_accept = input ("Please enter the accept states seperated by comma : ")
     print (s_accept)
 
@@ -101,15 +110,19 @@ def init_call ():
         print ("[!] Duplicate accept states please enter unique accept states!")
         init_call ()
 
+
     AcceptStates = FA_String_Parser.ParseToList (",",s_accept)
     if (not FA_String_Parser.CheckIfListContainsList (AcceptStates, states)):
         print ("[!] States were not found in state list!")
         init_call ()
 
+    return FiniteAuto(states, alphabet, s_start, AcceptStates)
+
+
 
 # create a finite auto class to process and create a table
 
 
-init_call ()
+fa = init_call ()
 
-fa = FiniteAuto(states, alphabet, startpoint, AcceptStates)
+#print (fa.startpoint)
